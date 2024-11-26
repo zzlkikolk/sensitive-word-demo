2acedaed0262f79c5c4070679eafed3c757097d3
zhangzhilin <zhangzhilin@igamebuy.com>
2024-11-26 16:21:01 +0800

    first commit

---
+++ src/main/java/org/example/sensitiveworddemo/SensitiveWordDemoApplication.java
@@ -0,0 +1,13 @@
+package org.example.sensitiveworddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensitiveWordDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SensitiveWordDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/sensitiveworddemo/SensitiveWordDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sensitiveworddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensitiveWordDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
